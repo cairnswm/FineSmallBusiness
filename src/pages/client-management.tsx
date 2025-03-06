af03fd403592e54cd51d7d97d0af18a422c2312e
fine-agent <fine@fine.dev>
2025-03-06 19:23:47 +0000

    Updated / Created files

--- src/pages/client-management.tsx
+++ src/pages/client-management.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDashboardContext } from "@/context/DashboardContext";
 import ClientCard from "@/components/dashboard/ClientCard";
 import AddClientModal from "@/components/dashboard/AddClientModal";
